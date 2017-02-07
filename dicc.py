6695e67d79c1b1fc10307f66f5320fdedc5e913e
FerFeuerFrei <f.torrijos@alumnos.urjc.es>
2017-02-07 11:26:36 +0100

    Dicc

--- dicc.py
+++ dicc.py
@@ -1,1 +1,19 @@
 # 13.5 Diccionario
+fich = open("/etc/passwd","r")
+# LISTA DE LINEAS
+lineas = fich.readlines()
+dicc = {}
+
+for linea in lineas:
+        login = linea.split(':')[0]
+        shell = linea.split(':')[-1][:-1]
+        print(login, shell)
+
+print"La shell de root:, dicc["root"]
+
+try:
+    print(dicc["imaginario"])
+except:
+    print("imaginario no existe.")
+    
+    
